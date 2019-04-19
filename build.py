d0ddac44b1c10b6c8e47c5e476e77f41e7190f26
sourav <souravyuvrajj@gmail.com>
2019-04-20 04:10:02 +0530

    final work

---
+++ build.py
@@ -0,0 +1,17 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.distutils")
+use_plugin("pypi:pybuilder_django_enhanced_plugin")
+
+name = "django_pybuilder_test"
+default_task = "publish"
+
+@init
+def set_properties(project):
+    project.set_property('django_project', 'RAFC')
+    project.set_property('django_apps', ['feedback'])
+    project.set_property('django_subpath', 'Project_RAFC')
